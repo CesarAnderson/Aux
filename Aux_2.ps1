418a09a7b066900afb37bbf20b8b6e2c7b73d677
Cesar <167170183+CesarAnderson@users.noreply.github.com>
2025-03-12 13:30:05 +0100

    Create Aux_2.ps1

---
+++ Aux_2.ps1
@@ -0,0 +1,25 @@
+
+$currentLocation = Get-Location
+
+
+if (Test-Path -Path "$currentLocation\Script") {
+    # Navegar a la carpeta Script
+    Set-Location -Path "$currentLocation\Script"
+    
+    
+    if (Test-Path -Path ".\venv\Scripts\activate.bat") {
+        & ".\venv\Scripts\activate.bat"
+        
+        
+        if (Test-Path -Path ".\modelo.py") {
+            python Procesar.py
+        } else {
+            Write-Host "Error: No se encuentra el archivo Procesar.py en la carpeta Script"
+        }
+    } else {
+        Write-Host "Error: No se encuentra el entorno virtual en la ruta Script\venv"
+    }
+} else {
+    Write-Host "Error: No se encuentra la carpeta Script en $currentLocation"
+    Write-Host "Este script debe ejecutarse desde la carpeta que contiene la subcarpeta Script"
+}
